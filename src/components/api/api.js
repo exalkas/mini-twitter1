9ae54e25fc912e290af52429bf1bc5fd31dc6498
alanmiste <baranalaan@gmail.com>
2021-11-06 10:55:22 +0100

    add api.js

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,13 +1,49 @@
 import axios from "axios";
 
 
-function api(){
+export async function MessagesApi(){ // to fetch all messages 
+
     const url = 'https://elak-mini-twitter.herokuapp.com/messages';
     
-    const response = axios.get(url);
+    const response = await axios.get(url);
     
-    console.log(response.data);
+    //  console.log('response.data');
+     
+     return response.data;
+    }
+    
+export async function OneMessageApi(id){ // to fetch one messages by it's ID
+        
+    const url = 'https://elak-mini-twitter.herokuapp.com/messages/'+id;
+        
+    const response = await axios.get(url);
+        
+    // console.log(response.data);
+   
+    return response.data;
 
-    return response;
+}
+
+export async function AllUsers(){ // to fetch all users
+        
+    const url = 'https://elak-mini-twitter.herokuapp.com/users';
+        
+    const response = await axios.get(url);
+        
+    // console.log(response.data);
+   
+    return response.data;
+
+}
+
+export async function UserById(id){ // to fetch one user by his-her ID
+        
+    const url = 'https://elak-mini-twitter.herokuapp.com/users/'+id;
+        
+    const response = await axios.get(url);
+        
+    // console.log(response.data);
+   
+    return response.data;
 
 }
