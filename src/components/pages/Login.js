f3f14188bff35bd0a5adba46953509c885312f62
alanmiste <baranalaan@gmail.com>
2021-11-06 14:34:15 +0100

    add Login page and ssome css

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-export default function Login(){
-    return(
-        <div>Login Page</div>
+export default function Login() {
+    return (
+        <div className='login'>
+            <img class="loginSideBar" src='https://abs.twimg.com/sticky/illustrations/lohp_en_1302x955.png'/>
+            <div class="loginComponents">
+                <h3 className='LoginInput'>Login or Signin </h3>
+                <input className='LoginInput' type="text" name="username" placeholder="Username" required />
+                <input className='LoginInput' type="password" name="password" placeholder="Password" required />
+                <div>
+                <input className='LoginBtn' type="submit" value="Login" />
+                <input className='LoginBtn' type="submit" value="Register" />
+                </div>
+            </div>
+        </div>
     )
 }
